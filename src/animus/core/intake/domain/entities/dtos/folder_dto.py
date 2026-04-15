4d2ce1c948fc212d1db6b19aa7c7d540f590e400
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-04-14 21:14:46 -0300

    fix: ANI-83 update FolderDto to include 'id' attribute initialization

--- src/animus/core/intake/domain/entities/dtos/folder_dto.py
+++ src/animus/core/intake/domain/entities/dtos/folder_dto.py
@@ -3,7 +3,7 @@ from animus.core.shared.domain.decorators import dto
 
 @dto
 class FolderDto:
+    id: str | None = None
     name: str
     account_id: str
     is_archived: bool = False
-    id: str | None = None
