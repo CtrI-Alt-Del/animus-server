9909fd3bb16111d3f15bad6f20de88ddc76c667e
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-05-01 17:47:51 -0300

    refactor: ANI-63 remove bertimbau embedding providers

--- src/animus/providers/intake/petition_summary_embeddings/bertimbau/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bertimbau_petition_summary_embeddings_provider import (
-    BertimbauPetitionSummaryEmbeddingsProvider,
-)
-
-__all__ = ['BertimbauPetitionSummaryEmbeddingsProvider']
--- src/animus/providers/intake/precedent_embeddings/bertimbau/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bertimbau_precedent_embeddings_provider import (
-    BertimbauPrecedentEmbeddingsProvider,
-)
-
-__all__ = ['BertimbauPrecedentEmbeddingsProvider']
