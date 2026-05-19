b1ac56845c2ddf3dde4c55b3d73270394bf01598
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-05-19 08:26:20 -0300

    feat: ANI-94 add unarchive method to Analysis class

--- src/animus/core/intake/domain/entities/analysis.py
+++ src/animus/core/intake/domain/entities/analysis.py
@@ -82,6 +82,9 @@ class Analysis(Entity):
 
     def archive(self) -> None:
         self.is_archived = Logical.create_true()
+        
+    def unarchive(self) -> None:
+        self.is_archived = Logical.create_false()
 
     def set_status(self, status: AnalysisStatus | str) -> None:
         if isinstance(status, str):
